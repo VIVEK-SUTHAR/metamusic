14a9b241827491fcedf4770bce3d00a85682cb7b
umangp31 <58567586+umangp31@users.noreply.github.com>
2022-08-07 01:39:30 +0530

    fea : modified

--- src/components/MusicPage.js
+++ src/components/MusicPage.js
@@ -28,30 +28,29 @@ export default function MusicPage() {
   const store = useStore();
   const currentAccount = store.currentAccount.slice(38);
   const [play, setPlay] = useState(false);
-  const allsong=store.allsong;
+  const allsong = store.allsong;
   console.log(allsong);
-  const {id}=useParams();
+  const { id } = useParams();
   console.log(allsong[id]);
 
-  
   // console.log(id);
   // // id ? (
   //   // console.log(currentSong);
   //  let currentSong= allsong[0]
   //   useEffect(()=>{
   //        currentSong= allsong[id];
-      
+
   // },[id])
   return (
     <div className="container relative">
       <div className="bg-[#2a2a2a]  h-16 w-full px-8 flex items-center justify-between">
         <div className="flex items-center justify-between h-full text-neutral-400 text-base font-medium">
           <ul className="flex">
-            <li>
+            <li
+              className="w-14 bg-transparent hover:bg-slate-400"
+              onClick={() => window.history.back()}
+            >
               <Previous />
-            </li>
-            <li className="ml-5">
-              <Next />
             </li>
           </ul>
         </div>
@@ -71,7 +70,9 @@ export default function MusicPage() {
         </div>
         <div className="px-8">
           {/* <h4 className="text-lg font-bold text-white">Song Type</h4> */}
-          <h1 className="font-extrabold text-7xl text-white">{allsong[id].songName}</h1>
+          <h1 className="font-extrabold text-7xl text-white">
+            {allsong[id].songName}
+          </h1>
           <p className="flex font-bold text-white">{allsong[id].singer}</p>
         </div>
       </div>
@@ -80,22 +81,22 @@ export default function MusicPage() {
         <div className=" ml-8 bg-green-500 hover:scale-110 ease-in-out duration-150 w-14 h-14 min-w-max min-h-max rounded-full gap-y-5 flex items-center justify-center">
           {play ? (
             <button onClick={() => setPlay((play) => !play)}>
-                  <audio
-                    // onTimeUpdate={changeValue}
-                    autoPlay={true}
-                    src={allsong[id].musicLink}
-                  ></audio>
-                  <PlayAtAlbum />
-                </button>
-              ) : (
-                <button onClick={() => setPlay((play) => !play)}>
-                  <PauseAtAlbum />
-                </button>
-              )}
+              <audio
+                // onTimeUpdate={changeValue}
+                autoPlay={true}
+                src={allsong[id].musicLink}
+              ></audio>
+              <PlayAtAlbum />
+            </button>
+          ) : (
+            <button onClick={() => setPlay((play) => !play)}>
+              <PauseAtAlbum />
+            </button>
+          )}
         </div>
 
         <a
-          href={`https://testnets.opensea.io/assets/mumbai/0x5E921d44C04A311d4cC98444513fD88554183E57/0`}
+          href={`https://testnets.opensea.io/assets/mumbai/0x5E921d44C04A311d4cC98444513fD88554183E57/${allsong[id].tokenID}`}
         >
           <div className="px-12">
             <div className="flex min-w-max items-center min-h-max bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 p-1 rounded-full">
@@ -109,12 +110,7 @@ export default function MusicPage() {
                 </div>
               </button>
             </div>
-            <RewardPopup
-              show={reward}
-              onClickOutside={() => {
-                setReward(false);
-              }}
-            />
+            
           </div>
         </a>
 
@@ -150,22 +146,28 @@ export default function MusicPage() {
         <div className="hover:bg-[#a7a7a7] tracking-widest font-Abeezee drop-shadow-lg ease-in-out duration-200 h-12 mt-4 mx-8 rounded-md flex items-center">
           <div className="list-decimal tracking-widest mx-8 flex font-medium text-lg text-white">
             {play ? (
-                <button className="mx-2 " onClick={() => setPlay((play) => !play)}>
-                  <audio
-                    // onTimeUpdate={changeValue}
-                    autoPlay={true}
-                    src={allsong[id].musicLink}
-                  ></audio>
-                  <Play />
-                </button>
-              ) : (
-                <button className="mx-2 " onClick={() => setPlay((play) => !play)}>
-                  <span className="pr-2">1. </span> 
-                  {/* <Pause /> */}
-                  {/* <Play/> */}
-                </button>
-              )}
-              <h1 className="">{allsong[id].songName}</h1>
+              <button
+                className="mx-2 "
+                onClick={() => setPlay((play) => !play)}
+              >
+                <audio
+                  // onTimeUpdate={changeValue}
+                  autoPlay={true}
+                  src={allsong[id].musicLink}
+                ></audio>
+                <Play />
+              </button>
+            ) : (
+              <button
+                className="mx-2 "
+                onClick={() => setPlay((play) => !play)}
+              >
+                <span className="pr-2">1. </span>
+                {/* <Pause /> */}
+                {/* <Play/> */}
+              </button>
+            )}
+            <h1 className="">{allsong[id].songName}</h1>
           </div>
         </div>
       </div>
