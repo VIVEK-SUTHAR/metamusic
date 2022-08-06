cd222f4d226fd7416d5cf49c37d0df0204c63320
VIVEK <sutharvivek18@gmail.com>
2022-08-06 10:03:55 +0530

    chore : front-end hero updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -22,26 +22,26 @@ const Hero = ({ connectWallet }) => {
     return (
         <div className="min-h-screen glass min-w-full">
             <div id="nav" className="flex flex-row justify-between top-0 mt-0 px-12">
-                <img src={MetaMask} alt="" className="w-32 invert-180"  />
+                <img src={MetaMask} alt="" className="w-32 image" />
                 <ul className="flex flex-row items-center justify-center transition-all ease-in-out duration-300 text-2xl font-Barlow font-light ">
-                    <li className="mx-6 my-2 text-[#606060] " >Home</li>
-                    <li className="mx-6 my-2 text-[#606060] " >About</li>
-                    <li className="mx-6 my-2 text-[#606060] " ><button className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-2 px-3 rounded-xl hover:scale-94 text-white" onClick={connectWallet}>Connect</button></li>
+                    <li className="mx-6 my-2 text-white font-mono " >Home</li>
+                    <li className="mx-6 my-2 text-white font-mono " >About</li>
+                    <li className="mx-6 my-2 text-white font-mono " onClick={connectWallet} ><button className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-2 px-3 rounded-xl hover:scale-94 text-white" >Connect</button></li>
                 </ul>
             </div>
-                {/* <hr className="h-1 w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"/> */}
+            {/* <hr className="h-1 w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"/> */}
             <div className="flex justify-around">
-            <main className=" bg-gradient-to-b pl-12 flex flex-col justify-center overflow-hidden rounded-lg mt-24">
-                <h2 className="font-extrabold max-w-4xl md:text-8xl font-Inter bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent ">Turn Your Music Into NFT's</h2>
-                <h2 className="text-5xl font-Dosis my-10 font-extrabold tracking-widest ">Listen | Own | Earn</h2>
-                <div className="flex justify-between max-w-sm">
-                <button className="px-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 max-w-xs font-semibold py-4 rounded-2xl hover:scale-94 text-2xl text-white" onClick={connectWallet}>Connect Wallet </button>
-                <button className="px-4 border-black border-2 max-w-xs font-semibold py-4 rounded-2xl hover:scale-94 text-2xl text-black hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:text-white hover:border-white" onClick={connectWallet}>Learn More </button>
+                <main className=" bg-gradient-to-b pl-12 flex flex-col justify-center overflow-hidden rounded-lg mt-24">
+                    <h2 className="font-extrabold max-w-4xl md:text-8xl font-Inter bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent ">Turn Your Music Into NFT's</h2>
+                    <h2 className="text-5xl font-QuickSand text-purple-500 font-extrabold my-10  tracking-widest ">Listen | Own | Earn</h2>
+                    <div className="flex justify-between max-w-sm">
+                        <button className="px-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 max-w-xs font-semibold py-4 rounded-2xl hover:scale-94 text-2xl text-white hover:scale-105 transition-all" onClick={connectWallet}>Connect Wallet </button>
+                        <button className="px-4 border-purple-400 border-2 max-w-xs font-semibold py-4 rounded-2xl hover:scale-94 text-2xl text-purple-500 hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:text-white hover:border-white hover:scale-105 transition-all" onClick={connectWallet}>Learn More </button>
+                    </div>
+                </main>
+                <div className="max-w-xl mt-4">
+                    <img src={illustration} alt="" />
                 </div>
-            </main>
-            <div className="max-w-xl mt-4">
-                <img src={illustration} alt="" />
-            </div>
             </div>
             {/* <section className="min-h-screen glass mx-2 rounded-xl flex flex-col sm:flex-row items-center justify-evenly sm:justify-around">
                 <main className=" rounded-full h-52 w-52  sm:h-72  sm:w-72   gap-3 glass border-0 border-green-200  flex justify-center items-center p-2 ">
