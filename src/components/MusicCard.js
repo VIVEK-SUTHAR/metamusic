3b17a500ef7b4cef2ad62151895bad5e13d8e588
Harsh2220 <harshgajjar145@gmail.com>
2022-08-06 22:15:08 +0530

    button removed

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,26 +1,7 @@
-import { useEffect } from "react";
-import { Pause } from "../Assets/Icons";
-
 export default function MusicCard({ image, name, singer }) {
-  useEffect(() => {
-    const MusicCard = document.getElementsByClassName("MusicCard");
-    for (let i = 0; i < MusicCard.length; i++) {
-      MusicCard[i].addEventListener("mouseover", () => {
-        document
-          .getElementsByClassName("PlayIcon")
-          [i].classList.remove("hidden");
-        // document.getElementsByClassName("PlayIcon")[i].classList.add("animate-ping")
-      });
-    }
-    for (let i = 0; i < MusicCard.length; i++) {
-      MusicCard[i].addEventListener("mouseleave", () => {
-        document.getElementsByClassName("PlayIcon")[i].classList.add("hidden");
-      });
-    }
-  });
   return (
     <div className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 p-1 rounded-md">
-      <div className="MusicCard min-w-max h-72 bg-[#181818] rounded-md p-4 overflow-hidden bg-gray-800 opacity-9 hover:transition-all hover:delay-75 gradientborder">
+      <div className="MusicCard min-w-max h-64 bg-[#181818] rounded-md p-4 overflow-hidden opacity-9 hover:transition-all hover:delay-75 gradientborder">
         <div className="w-40 h-40 rounded-md ">
           <img
             className="rounded-md"
@@ -32,9 +13,6 @@ export default function MusicCard({ image, name, singer }) {
             alt=""
             class="mMx2LUixlnN_Fu45JpFB SKJSok3LfyedjZjujmFt"
           ></img>
-          <div className="PlayIcon hidden relative mx-28 animate-bounce -my-12 w-10 h-10 bg-green-400 rounded-full flex items-center justify-center">
-            <Pause />
-          </div>
         </div>
         <div className="pt-4 text-white">
           <h1 className="text-lg">{name}</h1>
