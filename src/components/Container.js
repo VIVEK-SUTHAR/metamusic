6d11f69a187c693b2b53f2a28c5c1978682a8f01
umangp31 <58567586+umangp31@users.noreply.github.com>
2022-08-07 03:35:14 +0530

    fea: modified

--- src/components/Container.js
+++ src/components/Container.js
@@ -9,20 +9,22 @@ export default function Container() {
   const state= useStore();
   const setAllsong= state.setAllsong;
   const [allSongs, setallSongs] = useState([]);
-  
+  const allsong = state.allsong;
+console.log(allsong);
+
   useEffect(() => {
     getAllSongs().then(res => {
       setallSongs(res);
       setAllsong(res);
     });
     // console.log(allSongs);
-  }, []);
+  },);
 
   return (
     <div className="bg-[#121212] flex-1 py-6 min-h-full">
-      <Genre />
+      {/* <Genre /> */}
       <div className="w-screen flex flex-row flex-wrap justify-center gap-10 overflow-x-scroll Scroll ml-3 mb-24">
-        {allSongs.map((item, index) => (
+        {allsong.map((item, index) => (
           <Link to= {`/${index}`}>
             <MusicCard image={item.bannerImage} name={item.songName} singer={item.singer}/>
           </Link>
