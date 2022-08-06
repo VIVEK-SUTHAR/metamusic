fd57010429f820c080e344255eaed3a43104de2a
Harsh2220 <harshgajjar145@gmail.com>
2022-08-06 16:16:45 +0530

    conflict resolved

---
+++ src/components/Container.js
@@ -0,0 +1,28 @@
+import MusicCard from "./MusicCard";
+import getAllSongs from "../utils/getAllSongs";
+import Genre from "./Genre";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function Container() {
+  useEffect(() => {
+    getAllSongs();
+  }, []);
+
+  let MusicList = [];
+  for (let i = 0; i < 10; i++) {
+    MusicList[i] = i + 10;
+  }
+  return (
+    <div className="bg-[#121212] flex-1 overflow-y-scroll Scroll py-6 min-h-full">
+      <Genre />
+      <div className="w-screen flex flex-row flex-nowrap justify-start gap-10 overflow-x-scroll Scroll ml-3">
+        {MusicList.map((item) => (
+          <Link to={"1"}>
+            <MusicCard key={item} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
