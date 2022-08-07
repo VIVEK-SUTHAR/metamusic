27783c8f1e2021beb6cf4afbe5f424cb979a348d
VIVEK SUTHAR <sutharvivek18@gmail.com>
2022-08-07 05:31:30 +0530

    fea : env

--- src/utils/saveMetaData.js
+++ src/utils/saveMetaData.js
@@ -3,8 +3,7 @@ import { useState } from "react";
 import { NFTStorage } from "nft.storage";
 /// used NFT.storage to prepare the metadata for the NFT
 export const saveMetaData = async (image, Name, audioCID, singer) => {
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDM1MDk3QzQyMTAwY0QyNGYyODdBNEM4RUQ3MmIyYkVkMTczMzNjNzgiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1OTU5NzI3OTY0NCwibmFtZSI6Ik1ZS0VZIn0.VQGeFmV9xrlW8Ag5EKpX4GqnTY40ekTCl0R3m3YbPjI";
+  const token = process.env.REACT_APP_KEY;
   console.log("Preparing Metadata ....");
   const nft = {
     image: image,
